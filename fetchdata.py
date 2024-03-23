2c15040309aeee25947b6340859d0d67a72a738e
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2024-03-23 23:52:53 +0530

    figured out how to access the components

--- fetchdata.py
+++ fetchdata.py
@@ -17,14 +17,20 @@ print("links imported")
 
 for link in build_links[1:6]:
     with SB(uc_cdp=True, guest_mode=True) as sb:
-            sb.open(link)
-            try:
-                verify_success(sb)
-            except Exception:
-                if sb.is_element_visible('input[value*="Verify"]'):
-                    sb.click('input[value*="Verify"]')
-                elif sb.is_element_visible('iframe[title*="challenge"]'):
-                    sb.switch_to_frame('iframe[title*="challenge"]')
-                    sb.click("span.mark")
-                else:
-                    raise Exception("Detected!")
+        sb.open(link)
+        try:
+            verify_success(sb)
+        except Exception:
+            if sb.is_element_visible('input[value*="Verify"]'):
+                sb.click('input[value*="Verify"]')
+            elif sb.is_element_visible('iframe[title*="challenge"]'):
+                sb.switch_to_frame('iframe[title*="challenge"]')
+                sb.click("span.mark")
+            else:
+                raise Exception("Detected!")
+        
+        html = sb.get_page_source()
+        soup = BeautifulSoup(html, 'lxml')
+
+        
+    
